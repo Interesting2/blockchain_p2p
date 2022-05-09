898f2d8d8a6263ff8047f43b089e926a22c7589f
MetaCoderX <softwareguyhuron@gmail.com>
2022-05-10 00:14:16 +1000

    git init / boilerplate

---
+++ Blockchain.py
@@ -0,0 +1,56 @@
+import hashlib
+import json
+from time import time
+import datetime
+
+class Blockchain():
+    def  __init__(self):
+        self.blockchain = []
+        self.pool = []
+        self.poolLimit = 3
+        self.newBlock(previousHash="The Times 03/Jan/2009 Chancellor on brink of second bailout for banks.", proof=100)
+
+    def newBlock(self, proof, previousHash = None):
+        block = {
+            'index': len(self.blockchain) + 1,
+            'timestamp': str(datetime.datetime.now()),
+            'transaction': self.pool,
+            'proof': proof,
+            'previousHash': previousHash or self.calculateHash(self.blockchain[-1]),
+            'currentHash': self.currentHash(self.pool)
+        }
+        self.pool = []
+        self.blockchain.append(block)
+    
+    def lastBlock(self):
+        return self.blockchain[-1]
+
+    def calculateHash(self, block):
+        blockObject = json.dumps(block, sort_keys=True)
+        blockString = blockObject.encode()
+        rawHash = hashlib.sha256(blockString)
+        hexHash = rawHash.hexdigest()
+        print("Prev hash: ", hexHash)
+        return hexHash
+    
+    def addTransaction(self, transaction):
+        if len(self.pool) < self.poolLimit:
+            self.pool.append(transaction)
+        lastBlock = self.lastBlock()
+        return lastBlock['index'] + 1
+        
+    def currentHash(self, pool):
+        currentTransactions = ""
+        if len(pool) > 0:
+            previousHash = self.calculateHash(self.lastBlock())
+            currentTransactions += previousHash
+            for transaction in pool:
+                currentTransactions += transaction
+            return hashlib.sha256(currentTransactions.encode()).hexdigest()
+
+blockchain = Blockchain()
+t1 = blockchain.addTransaction("Satoshi|Mike|5 BTC")
+t2 = blockchain.addTransaction("Mike|Satoshi|1 BTC")
+t3 = blockchain.addTransaction("Satoshi|Hal Finney|5 BTC")
+blockchain.newBlock(12345)
+print("Genesis block: ", blockchain.blockchain)
---
+++ BlockchainMiner.py
@@ -0,0 +1,20 @@
+from _thread import *
+import threading
+import time
+import socket
+import _thread
+import sys
+import json
+import hashlib
+import time
+
+class BlockchainMiner():
+    def __init__(self, *args):
+        pass
+    
+    def run(self):
+        pass
+    
+    def proof_of_work(self):
+        pass
+
